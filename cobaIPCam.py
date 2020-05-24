6bf18fb98c14b2a3b43097e996476857deb8d33c
FaizDhiaulhaq <48075328+FaizDhiaulhaq@users.noreply.github.com>
2020-05-24 14:17:21 +0700

    Add files via upload

---
+++ cobaIPCam.py
@@ -0,0 +1,19 @@
+import numpy
+import cv2
+
+cap = cv2.VideoCapture('rtsp://admin:FauziFaiz@192.168.1.64/1')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+font =  cv2.FONT_HERSHEY_SIMPLEX
+while cap.isOpened():
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 2.2, 5)
+
+    for (x, y ,w ,h) in faces:
+        cv2.putText(img,"Wajah",(x,y-10),font,0.75,(0,255,0),2,cv2.LINE_AA)
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+#cap.release()
+cv2.destroyAllWindows()
